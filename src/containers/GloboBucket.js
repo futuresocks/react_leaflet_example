a9e050bda79abeb0f487266651aeae2d2a5132cd
Colin Bell <colin@dogoodercomics.co.uk>
2018-08-16 22:11:47 +0100

    adds navbar

--- src/containers/GloboBucket.js
+++ src/containers/GloboBucket.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import GlobeView from './GlobeView'
+import Navbar from '../components/Navbar'
 
 const GloboBucket = () => (
+
   <Router>
-    <Route exact path="/" component= { GlobeView }/>
+    <React.Fragment>
+      <Navbar/>
+      <Route exact path="/" component= { GlobeView }/>
+    </React.Fragment>
   </Router>
+
 )
 
 export default GloboBucket
