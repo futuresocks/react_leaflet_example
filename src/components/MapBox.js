d5b42a32fccc0a063d67903673e8fa655a19b2e0
Colin Bell <colin@dogoodercomics.co.uk>
2018-08-16 11:16:07 +0100

    Adds adding and removing marker functionality

--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -2,21 +2,43 @@ import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
 import './MapBox.css'
 
-const MapBox = (props) => {
+class MapBox extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      markers: []
+    }
+    this.addMarker = this.addMarker.bind(this)
+    this.removeMarker = this.removeMarker.bind(this)
+  }
 
-  if(!props.country) return null
+  addMarker = function(event){
+    this.setState({markers: [...this.state.markers, event.latlng]})
+  }
 
-  return(
-  <Map center={props.country.latlng} zoom={1}>
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-    />
-    <Marker position={props.country.latlng}>
-      <Popup>A pretty CSS3 popup.<br />Easily customizable.</Popup>
-    </Marker>
-  </Map>
-)
+  removeMarker = function(event){
+    let markers = this.state.markers.filter(coords => coords !== event.latlng);
+    this.setState({markers})
+  }
+
+
+  render(){
+    if(!this.props.country) return null
+
+    const markerNodes = this.state.markers.map((coords, index) => {
+      return <Marker key={index} position={coords} onClick ={this.removeMarker}/>
+    })
+
+    return(
+      <Map center={this.props.country.latlng} zoom={1} onClick={this.addMarker}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        />
+        {markerNodes}
+      </Map>
+    )
+  }
 
 }
 
